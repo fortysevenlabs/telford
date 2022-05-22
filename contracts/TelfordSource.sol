76100bf2c0b89bf5dbd9c246c141625cfde2126f
Gates Porter <gatesporter8@Gatess-Air.attlocal.net>
2022-05-22 00:32:14 -0500

    some cleanup

--- contracts/TelfordSource.sol
+++ contracts/TelfordSource.sol
@@ -4,13 +4,10 @@ pragma solidity ^0.8.0;
 contract TelfordSource {
     // this contract will be deployed to Arbitrum
     address private userAddress;
-    address private bonderClient;
+    address private bonderAddress;
     address private l1Relayer;
-
-    uint256 private requestedTransferAmount;
-    uint256 private bonderPayment;
-
     uint256 constant bonderFee = 0.2 ether;
+    uint256 private bonderPayment;
 
     event BridgeRequested(uint256 indexed amount);
     event TransferRequestBonded(address indexed userAddress);
@@ -25,35 +22,36 @@ contract TelfordSource {
     }
     modifier onlyBonder() {
         require(
-            msg.sender == bonderClient,
+            msg.sender == bonderAddress,
             "Sorry pal, I can only be called by the Bonder!"
         );
         _;
     }
 
-    constructor(address _bonderClientAddress, address _l1RelayerAddress) {
-        bonderClient = _bonderClientAddress;
+    constructor(address _bonderAddress, address _l1RelayerAddress) {
+        bonderAddress = _bonderAddress;
         l1Relayer = _l1RelayerAddress;
     }
 
     function bridge(uint256 _amount) external {
         userAddress = msg.sender;
-        requestedTransferAmount = _amount;
-
-        emit BridgeRequested(requestedTransferAmount);
+        bonderPayment = _amount + bonderFee;
+        //
+        // To Do. Access the WETH contract, and approve this contract address to transfer bonderPayment from userAddress.
+        //
+        emit BridgeRequested(_amount);
     }
 
     function bond() external onlyBonder {
-        bonderPayment = requestedTransferAmount + bonderFee;
         //
-        // To Do. logic to transfer the _bonderPayment from the _userAddress on Arbitrum to the contract account.
+        // To Do. logic to transfer the bonderPayment from the userAddress to this contract address.
         //
         emit TransferRequestBonded(userAddress);
     }
 
     function fundsReceivedOnDestination() external onlyL1Relayer {
         //
-        // To Do. logic to transfer the _bonderPayment to the _bonderClient on Arbitrum from this contract.
+        // To Do. logic to transfer the bonderPayment from this contract address to the bonderAddress.
         //
         emit BonderReimbursed(bonderPayment);
     }
