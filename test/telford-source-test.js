8ebf93539fa63e44a5936ecf7117b87ef3b8d90c
Gates Porter <gatesporter8@Gatess-Air.attlocal.net>
2022-05-22 21:05:45 -0500

    proper variable names

--- test/telford-source-test.js
+++ test/telford-source-test.js
@@ -3,50 +3,50 @@ const { ethers } = require("hardhat");
 
 describe("TelfordSource", function () {
 	let telfordSource;
-	let owner;
-	let account1;
-	let account2;
-	let account3;
+	let user;
+	let bonder;
+	let l1Relayer;
+	let otherAccount;
 
 	beforeEach("setup accounts", async () => {
 		const accounts = await ethers.getSigners();
 
-		owner = accounts[0];
-		account1 = accounts[1];
-		account2 = accounts[2];
-		account3 = accounts[3];
+		user = accounts[0];
+		bonder = accounts[1];
+		l1Relayer = accounts[2];
+		otherAccount = accounts[3];
 
 		const TelfordSource = await ethers.getContractFactory("TelfordSource");
-		telfordSource = await TelfordSource.deploy(account1.address, account2.address);
+		telfordSource = await TelfordSource.deploy(bonder.address, l1Relayer.address);
 		
 		await telfordSource.deployed();
 	});
 
 	describe("bridge", function () {
 		it("should revert when ether sent is less than or equal to the bonder fee", async function () {
-			await expect(telfordSource.connect(owner).bridge({value: 0}))
+			await expect(telfordSource.connect(user).bridge())
 				.to.be.revertedWith("Ether sent must be greater than the bonder fee!");
 		});
 
 		it("should emit an event", async function () {
-			await expect(telfordSource.connect(owner).bridge({value: 1000000000000000000n}))
+			await expect(telfordSource.connect(user).bridge({value: 1000000000000000000n}))
 				.to.emit(telfordSource, "BridgeRequested")
-				.withArgs(owner.address, 800000000000000000n);
+				.withArgs(user.address, 800000000000000000n);
 		});
 	});
 
 	describe("fundsReceivedOnDestination", function () {
 		it("should revert when not called by the L1Relayer", async function () {
-			await telfordSource.connect(owner).bridge({value: 1000000000000000000n});
+			await telfordSource.connect(user).bridge({value: 1000000000000000000n});
 
-			await expect(telfordSource.connect(account3).fundsReceivedOnDestination())
+			await expect(telfordSource.connect(otherAccount).fundsReceivedOnDestination())
 				.to.be.revertedWith("Sorry pal, I can only be called by the L1Relayer!");
 		});
 
 		it("should emit an event", async function () {
-			await telfordSource.connect(owner).bridge({value: 1000000000000000000n});
+			await telfordSource.connect(user).bridge({value: 1000000000000000000n});
 
-			await expect(telfordSource.connect(account2).fundsReceivedOnDestination())
+			await expect(telfordSource.connect(l1Relayer).fundsReceivedOnDestination())
 				.to.emit(telfordSource, "BonderReimbursed")
 				.withArgs(1000000000000000000n);
 		});
